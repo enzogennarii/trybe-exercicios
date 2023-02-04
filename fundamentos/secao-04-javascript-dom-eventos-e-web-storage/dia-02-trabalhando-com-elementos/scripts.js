0eab1c3bfe3550c1fce58b261497efd7f5b0a0f8
Enzo Gennari <enzo.gennari02@gmail.com>
2023-02-04 17:19:03 -0300

    Adiciona exercícios do dia 4.2

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/scripts.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/scripts.js
@@ -19,3 +19,19 @@ trdFilhoDeOndeEsta.innerText = 'Terceiro Filho - Elemento de onde está';
 
 const trdFilhoDePai = paiDoElementoOndeEsta.children[2];
 trdFilhoDePai.innerText = 'Terceiro Filho de pai';
+
+
+const pai = document.getElementById('pai');
+const irmaoOndeEsta = document.createElement('section');
+irmaoOndeEsta.innerText = 'Sou o irmão de onde está';
+irmaoOndeEsta.id = 'irmao-do-elemento-onde-esta';
+pai.appendChild(irmaoOndeEsta);
+
+const filhoDeOndeEsta = document.createElement('section');
+filhoDeOndeEsta.id = 'sou-um-filho-do-elemento-onde-esta';
+filhoDeOndeEsta.innerText = 'Filho Criado do Elemento onde está';
+elementoOndeEsta.appendChild(filhoDeOndeEsta);
+
+
+const elementoASerRemovido = document.querySelector('h2');
+elementoOndeEsta.removeChild(elementoASerRemovido);
