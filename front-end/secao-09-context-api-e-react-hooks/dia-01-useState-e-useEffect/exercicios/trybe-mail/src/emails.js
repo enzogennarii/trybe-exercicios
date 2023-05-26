d29e0e01d7f5551fe2fae921d8521acddd8b125a
Enzo Gennari <enzo.gennari02@gmail.com>
2023-05-26 17:01:02 -0300

    Comitão bomba de atividades do dia a dia da Trybe

---
+++ front-end/secao-09-context-api-e-react-hooks/dia-01-useState-e-useEffect/exercicios/trybe-mail/src/emails.js
@@ -0,0 +1,27 @@
+export const emailsList = [
+  {
+    id: 1,
+    title: "Email de: Nasc",
+    status: 0
+  },
+  {
+    id: 2,
+    title: "Email de: Carolzita",
+    status: 0
+  },
+  {
+    id: 3,
+    title: "Email de: Thalles",
+    status: 0
+  },
+  {
+    id: 4,
+    title: "Email de: Aninha",
+    status: 0
+  },
+  {
+    id: 5,
+    title: "Email de: Flávio",
+    status: 0
+  }
+];
